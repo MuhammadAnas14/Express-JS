6e4b79abaa0cee83f228c0cf21b830ec57b49794
anas124 <anasm9877@gmail.com>
2019-03-27 00:27:37 +0500

    api prac

--- prac/index5.js
+++ prac/index5.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const members= require('../../members');
+const members= require('./members');
 const path= require('path');
 
-app.use('/members',require('./routes/api/members'));
+app.use('/member',require('./routes/api/member'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.listen(3000,()=>{
   console.log('on scen hai');
--- prac/routes/api/member.js
+++ prac/routes/api/member.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const router = express.Router();
-const members= require('./members');
+const members= require('../../members');
 
 
 router.get('/',(req,res)=>{
@@ -9,15 +9,34 @@ router.get('/',(req,res)=>{
 });
 router.get('/:id',(req,res)=>{
   // res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  const found = members.some(member => member.id === parseInt(req.params.id));
+  const found = members.some(members => members.id === parseInt(req.params.id));
 
   if(found){
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    res.json(members.filter(members => members.id === parseInt(req.params.id)));
 
   }
   else{
     res.status(400).json({msg: 'memvber mnot fpound'});  }
 });
+
+//create members
+router.post('/',(req,res)=>{
+  const newMember= {
+    id: members.length +1,
+    name: req.body.name,
+    email: req.body.email,
+    status : 'jhund'
+  }
+
+  if(!newMember.name || !newMember.email){
+    return res.status(400).json({ msg : 'name aur email enter karo'});
+
+  }
+  members.push(newMember);
+  res.json(members);
+
+});
+
 
 module.exports =router;
 
